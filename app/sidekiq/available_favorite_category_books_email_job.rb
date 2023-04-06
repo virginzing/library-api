2e9ec61a40242003e5f1756cfee34fe4499d49ef
Tadthep Kanlayanapark <virginzing@gmail.com>
2023-04-06 21:18:45 +0700

    added sidekiq log

--- app/sidekiq/available_favorite_category_books_email_job.rb
+++ app/sidekiq/available_favorite_category_books_email_job.rb
@@ -5,6 +5,8 @@ class AvailableFavoriteCategoryBooksEmailJob
     User.all.each do |user| 
       recommend_books = Book.where(rent_by: nil, category: user.favorite_categories)
 
+      puts("AvailableFavoriteCategoryBooksEmailJob: mail to #{user.email} by categories = #{user.favorite_categories}")
+
       AvailableFavoriteCategoryBooksMailer.with(user: user, books: recommend_books).recommend_email.deliver_now
     end
   end
